9e53931a431fe4a7b6bc3d199bbf7af70827f08f
ASensiu <105224518+JordanGuevara@users.noreply.github.com>
2025-01-26 15:36:26 -0500

    Delete U2Lab2_ASO_GuevaraJordan directory

--- U2Lab2_ASO_GuevaraJordan/database/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    stock INT NOT NULL,
-    category VARCHAR(100) NOT NULL,
-    supplier VARCHAR(255) NOT NULL
-);
